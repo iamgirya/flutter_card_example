05b33420cf07a570f3043fc338392d7825d32a55
iamgirya <84345515+iamgirya@users.noreply.github.com>
2025-02-01 22:39:52 +0300

    Add error handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 import 'main_app.dart';
+import 'tools/error_handler.dart';
 import 'tools/logger.dart';
 
 void main() {
   initLogger();
   logger.info('Start main');
+  ErrorHandler.init();
 
   runApp(const MainApp());
 }
